1681e859af841c41d62d9daefddd177756da78d1
Sebastian Quintero <sebastianext@gmail.com>
2016-04-08 13:46:44 -0500

    Agregando id a cada campo del formulario de clientes para poder realizar pruebas unitarias

--- resources/views/cliente/forms/cliente.blade.php
+++ resources/views/cliente/forms/cliente.blade.php
@@ -7,7 +7,7 @@
 		    <div class="field padding-right-campo">
 			      <label>Nº Cedula</label>
 				    <div class="ui corner labeled input">
-				      {!! Form::text('cedula',null,['placeholder'=>'Nº Cedula']) !!}
+				      {!! Form::text('cedula',null,['placeholder'=>'Nº Cedula','id'=>'cedula']) !!}
 				      <div class="ui obligatorio corner label">
 					  			<i class="asterisk icon"></i>
 							</div>
@@ -19,7 +19,7 @@
 		    <div class="field">
 			      <label>Nombres</label>
 				    <div class="ui corner labeled input">
-				     {!! Form::text('nombres',null,['placeholder'=>'Nombres']) !!}
+				     {!! Form::text('nombres',null,['placeholder'=>'Nombres','id'=>'nombres']) !!}
 				      <div class="ui obligatorio corner label">
 					  			<i class="asterisk icon"></i>
 							</div>
@@ -29,7 +29,7 @@
 		    <div class="field">
 			      <label>Apellidos</label>
 				    <div class="ui corner labeled input">
-				      {!! Form::text('apellidos',null,['placeholder'=>'Apellidos']) !!}
+				      {!! Form::text('apellidos',null,['placeholder'=>'Apellidos','id'=>'apellidos']) !!}
 				      <div class="ui obligatorio corner label">
 					  			<i class="asterisk icon"></i>
 							</div>
@@ -42,7 +42,7 @@
 		    <div class="field padding-right-campo">
 			      <label>Direccion</label>
 				    <div class="ui corner labeled input">
-				      {!! Form::text('direccion',null,['placeholder'=>'Direccion']) !!}
+				      {!! Form::text('direccion',null,['placeholder'=>'Direccion','id'=>'direccion']) !!}
 				    </div>
 		    </div>
 		  </div>
@@ -50,7 +50,7 @@
 		    <div class="field padding-right-campo">
 			      <label>Telefono</label>
 				    <div class="ui corner labeled input">
-				      {!! Form::text('telefono',null,['placeholder'=>'3121234589-7450088']) !!}
+				      {!! Form::text('telefono',null,['placeholder'=>'3121234589-7450088','id'=>'telefono']) !!}
 				    </div>
 		    </div>
 		  </div>
@@ -59,7 +59,7 @@
 		    <div class="field padding-right-campo">
 			      <label>Correo</label>
 				    <div class="ui corner labeled input">
-				      {!! Form::text('correo',null,['placeholder'=>'correo@dominio.com']) !!}
+				      {!! Form::text('correo',null,['placeholder'=>'correo@dominio.com','id'=>'correo']) !!}
 				    </div>
 		    </div>
 		  </div>
--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -6,14 +6,68 @@ use Illuminate\Foundation\Testing\DatabaseTransactions;
 
 class ExampleTest extends TestCase
 {
+
     /**
-     * A basic functional test example.
+     *   Autor:Johan Sebastian Quintero
+     *   Versión: v1.0
+     *   Fecha: 08-04-2016 13:28
+     *   Descripción: Prueba unitaria de ingreso a la pagina principal
      *
      * @return void
      */
-    public function testBasicExample()
+    public function testInicioExample()
     {
         $this->visit('/')
-             ->see('hola');
+             ->see('Deposito la Quinta');
+    }
+
+    /**
+     *   Autor:Johan Sebastian Quintero
+     *   Versión: v1.0
+     *   Fecha: 08-04-2016 13:28
+     *   Descripción: Prueba unitaria de registro de cliente
+     *
+     * @return void
+     */
+    public function testRegistrarCliente()
+    {
+        $this->visit('/')
+             ->click('Clientes')
+             ->seePageIs('/cliente')
+             ->click('Crear Cliente')
+             ->seePageIs('/cliente/create')
+             ->type('1094941407','#cedula')
+             ->type('Johan Sebastian','#nombres')
+             ->type('Quintero Orozco','#apellidos')
+             ->type('Ciudadela el sol','#direccion')
+             ->type('3128049965','#telefono')
+             ->type('sebastianext@gmail.com','#correo')
+             ->press('Aceptar')
+             ->see('Accion Procesada!');        
+    }
+
+    /**
+     *   Autor:Johan Sebastian Quintero
+     *   Versión: v1.0
+     *   Fecha: 08-04-2016 13:28
+     *   Descripción: Prueba unitaria de registro de cliente
+     *
+     * @return void
+     */
+    public function testRegistrarProducto()
+    {
+        $this->visit('/')
+             ->click('Clientes')
+             ->seePageIs('/cliente')
+             ->click('Crear Cliente')
+             ->seePageIs('/cliente/create')
+             ->type('1094941407','#cedula')
+             ->type('Johan Sebastian','#nombres')
+             ->type('Quintero Orozco','#apellidos')
+             ->type('Ciudadela el sol','#direccion')
+             ->type('3128049965','#telefono')
+             ->type('sebastianext@gmail.com','#correo')
+             ->press('Aceptar')
+             ->see('Accion Procesada!');        
     }
 }
